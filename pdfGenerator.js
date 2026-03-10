86a2e25fc798429d8d88935c87bcb07b8efda581
monirul2202143 <moni017803@gmail.com>
2026-03-10 13:41:57 +0600

    Refactor pdfGenerator.js for improved readability

--- pdfGenerator.js
+++ pdfGenerator.js
@@ -109,7 +109,7 @@ form.addEventListener('submit', function (event) {
     Rajshahi University of Engineering and Technology
   </h1>
 
-  <img src="./ruet-monogram-4176-x-5000.png"
+  <img src="ruet-monogram-4176-x-5000.png"
        alt="RUET Logo"
        id="ruetLogo">
 
@@ -143,7 +143,7 @@ form.addEventListener('submit', function (event) {
   <h3 id="submission">Submission Date: ${getFormattedDate(date.value)}</h3>
 
   <img id="backgroundImage"
-       src="./ruet-monogram-4176-x-5000.png"
+       src="ruet-monogram-4176-x-5000.png"
        alt="background image">
       `
     btnDiv.innerHTML = '<button onclick="generatePDF()">Download PDF</button><button onclick = "reset()"> Reset</button>';
@@ -187,3 +187,4 @@ const getFormattedDate = (dateInput) => {
   return formattedDate;
 
 }
+
